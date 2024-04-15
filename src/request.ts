21306a45f2d4176748a15a411055ad2fafcc58da
oddyamill <43600905+oddyamill@users.noreply.github.com>
2024-04-15 20:22:17 +0700

    user agent header

--- src/request.ts
+++ src/request.ts
@@ -1,7 +1,13 @@
 import { RouteBases } from 'discord-api-types/v10'
 
-export async function request<T>(pathname: string, init?: RequestInit) {
-  const response = await fetch(RouteBases.api + pathname, init)
+export async function request<T>(pathname: string, init: RequestInit = {}) {
+  const response = await fetch(RouteBases.api + pathname, {
+    ...init,
+    headers: {
+      'User-Agent': 'DiscordBot (https://github.com/oddyamill/discord-workers, 0.0.0)',
+      ...(init.headers ?? {}),
+    },
+  })
 
   if (!response.ok) {
     throw new Error(`Request failed: ${response.status} ${response.statusText}`)
